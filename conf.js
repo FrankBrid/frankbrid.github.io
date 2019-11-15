b85e4b6f33a849cd61c39e4561fe5c4590235de1
FrankBrid <50645157+FrankBrid@users.noreply.github.com>
2019-11-15 14:48:49 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,58 +2,44 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '119',
+  ver: '111',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
-    enable: true,
-    ver: 80
+    enable: false,
+    ver: 62
   },
-
-    // 节点配置
+  // 节点配置
   node_map: {
     'mysite': {
-      label: '当前站点',
+      label: '美国节点',
       lines: {
-        [location.host]: 1,
-      }
-    },
-    'us': {
-      label: '美国线路',
-      lines: {
+        // 主机:权重
         'node-us1.bbs8080.net:8443': 1,
-        'node-us2.bbs8080.net:8443': 1,       
+        'node-us2.bbs8080.net:8443': 1,
       }
     },
-     'hk': {
-      label: '香港节点',
-      //hidden: true,
-      lines: {
-        'node-hk.bbs8080.net:8443': 1,
-      }
-    }
   },
 
   /**
    * 默认节点
    */
-   node_default: 'mysite',
-  //node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite',
+  //node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'us' : 'us',
 
   /**
    * 加速节点
    */
-  node_acc: 'hk',
+  node_acc: 'us',
 
   /**
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
   //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
-    assets_cdn: 'https://cdn.jsdelivr.net/gh/frankbrid/frankbrid.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+  assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
@@ -72,9 +58,6 @@ jsproxy_config({
   url_handler: {
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
     },
     'http://haha.com/': {
       content: 'Hello World'

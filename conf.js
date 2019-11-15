73ce9c20951d6e075cfe59785b501ce20b38a570
FrankBrid <50645157+FrankBrid@users.noreply.github.com>
2019-11-15 14:28:38 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,43 +2,34 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '118',
+  ver: '119',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 79
+    ver: 80
   },
 
-  // 节点配置
+    // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
     'mysite': {
       label: '当前站点',
       lines: {
         [location.host]: 1,
       }
     },
-    // 该节点用于加载大体积的静态资源
-    'cfworker': {
-      label: '',
-      hidden: true,
+    'us': {
+      label: '美国线路',
       lines: {
-        'node-cfworker-2.etherdream.com': 1,
+        'node-us1.bbs8080.net:8443': 1,
+        'node-us2.bbs8080.net:8443': 1,       
+      }
+    },
+     'hk': {
+      label: '香港节点',
+      //hidden: true,
+      lines: {
+        'node-hk.bbs8080.net:8443': 1,
       }
     }
   },
@@ -46,19 +37,20 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  // node_default: 'mysite',
-  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
+   node_default: 'mysite',
+  //node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
    */
-  node_acc: 'cfworker',
+  node_acc: 'hk',
 
   /**
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+    assets_cdn: 'https://cdn.jsdelivr.net/gh/frankbrid/frankbrid.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
   // assets_cdn: 'assets/',
